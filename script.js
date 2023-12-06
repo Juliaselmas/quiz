5ab3746e0c673a49370cda9782ca21cb2300a11d
Juliaselmas <144105839+Juliaselmas@users.noreply.github.com>
2023-12-06 12:03:14 +0100

    made it possible to loop through all questions

--- script.js
+++ script.js
@@ -4,15 +4,61 @@ let questions = [
         correctAnswer: false,
         id:"question1"
     },
+    {
+        question: "Är vattnet blött?",
+        correctAnswer: true,
+        id:"question2"
+    },
+    {
+        question: "Är jorden platt?",
+        correctAnswer: true,
+        id:"question3"
+    },
+    {
+        question: "Är vattnet blött?",
+        correctAnswer: true,
+        id:"question4"
+    },
+    {
+        question: "Är jorden platt?",
+        correctAnswer: false,
+        id:"question5"
+    },
+    {
+        question: "Är vattnet blött?",
+        correctAnswer: true,
+        id:"question6"
+    },
+    {
+        question: "Är vattnet blött?",
+        correctAnswer: false,
+        id:"question7"
+    },
+    {
+        question: "Är jorden platt?",
+        correctAnswer: true,
+        id:"question8"
+    },
+    {
+        question: "Är vattnet blött?",
+        correctAnswer: false,
+        id:"question9"
+    },
+    {
+        question: "Är jorden platt?",
+        correctAnswer: false,
+        id:"question10"
+    },
   ];
 
 let darkModeBtn = document.querySelector("#darkMode");
+let darkModeLabel = document.querySelector("#darkMode-container label")
 let startBtn = document.querySelector("#startButton");
 let quizQuestion = document.querySelector("#question-container");
-//let quizQuestion = document.getElementById("question-container");
 
 darkModeBtn.addEventListener("change", () => {
     document.body.classList.toggle("dark", darkModeBtn.checked);
+    darkModeLabel.innerText = darkModeBtn.checked ? "Light mode, please:" : "Dark mode, please:";
 });
 
 //funktion för att skapa ny fråga
@@ -57,12 +103,52 @@ function createQuestion(question) {
 };
 
 
-//funktion för att starta quizet
+let currentQuestionIndex = 0;
+
+//funktion för att starta quizet:
 startBtn.addEventListener("click", () => {
+
+    //döljer föregående fråga om det finns en:
+    if (quizQuestion.firstChild) {
+        quizQuestion.firstChild.style.display = "none";
+    }
+    //visar en ny fråga:
+    if (currentQuestionIndex < questions.length) {
+        let quizElement = createQuestion(questions[currentQuestionIndex]);
+        quizQuestion.innerHTML = "";
+        quizQuestion.appendChild(quizElement);
+
+        quizQuestion.firstChild.style.display = "block";
+
+        //ändrar texten på knappen:
+        if (currentQuestionIndex === questions.length - 1) {
+            startBtn.innerText = "Show Result";
+        } else {
+            startBtn.innerText = "Next Question";
+        }
+
+        //ökar indexet för att hålla räkningen på frågorna:
+        currentQuestionIndex++;
+
+    } else {
+        //kör funktion för att visa resultatet om frågorna är slut:
+        showResult();
+    };
+});
+
+//skapa en funktion för att visa resultatet här:
+function showResult(){
+
+};
+
+
+/*startBtn.addEventListener("click", () => {
+    quizQuestion.innerHTML = "";
+
     questions.forEach((question) => {
         let quizElement = createQuestion(question);
         quizQuestion.append(quizElement);
     });
 
-    startBtn.innerText = "NEXT";
-});
+    startBtn.innerText = "Next";
+});*/
